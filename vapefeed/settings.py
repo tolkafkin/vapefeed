53ab5a4c119daf8b0bd9140acc2b48baf3551d04
tolkafkin <tolkafkin@gmail.com>
2024-04-08 16:57:12 +0300

    update reqs, settings.py

--- vapefeed/settings.py
+++ vapefeed/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'feedbacks.apps.FeedbacksConfig',
+    'liquids.apps.LiquidsConfig',
+    'profiles.apps.ProfilesConfig',
+    'scores.apps.ScoresConfig',
+    'users.apps.UsersConfig',
+    'vapes.apps.VapesConfig',
 ]
 
 MIDDLEWARE = [
